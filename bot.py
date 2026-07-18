7dc9f5b0d3e6898dccc4f86ac262e88b7c7118c5
tareqaa555-beep <tareqaa555@gmail.com>
2026-07-18 19:43:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 level=logging.INFO,
 )
 
-TOKEN =8578143859:AAFZAqkgGdFgLBSmSZAGQJx0Hv50TeTn2ZM
+TOKEN =8578143859:8578143859:AAH0frQQvhuGr3z5qkwJL4eUsQk7DxxOq5s
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 await update.message.reply_text(
